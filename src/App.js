8e58741ac5a0ef7b602f6de98de5da624f082f0d
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-01-27 11:05:57 +0200

    sss

--- src/App.js
+++ src/App.js
@@ -520,7 +520,7 @@ function App() {
       }
       console.log('additionalParams====>', additionalParams);
       // Формування фінального лінку
-      const product = `${baseUrl}&${additionalParams}`;
+      const product = `${baseUrl}&${additionalParams}&yhugh=${pushOpenWebview}`;
       //(!addPartToLinkOnce ? `&yhugh=true` : ''); pushOpenWebview && '&yhugh=true'
       console.log('Фінальна лінка сформована');
 
